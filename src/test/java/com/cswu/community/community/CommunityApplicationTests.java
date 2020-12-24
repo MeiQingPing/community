b4bf05eb6eb829628179b3cd5734c48f14b9be3f
qp_mei <qp_mei@qq.com>
2020-12-24 10:01:46 +0800

    input your message

---
+++ src/test/java/com/cswu/community/community/CommunityApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cswu.community.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+    public static void main(String[] args){
+        SpringApplication.run(CommunityApplication.class,args);
+    }
+    @Test
+    void contextLoads() {
+    }
+
+}
